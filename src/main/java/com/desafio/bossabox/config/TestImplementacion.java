ddb1b84dbab96c102d873ef8cceedd3701f17451
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-18 15:25:49 -0300

    Test insert ferramentas in DB

---
+++ src/main/java/com/desafio/bossabox/config/TestImplementacion.java
@@ -0,0 +1,40 @@
+package com.desafio.bossabox.config;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.desafio.bossabox.entities.Ferramenta;
+import com.desafio.bossabox.repositoriries.FerramentaRepository;
+
+@Configuration
+@Profile("test")
+public class TestImplementacion implements CommandLineRunner{
+	
+	@Autowired
+	private FerramentaRepository ferramentaRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<String> list1 = Arrays.asList("organization","planning","collaboration",
+				"writing","calendar");
+		
+		List<String> list2 = Arrays.asList("api","json","schema","node","github","rest");
+		
+		Ferramenta f1 = new Ferramenta(null,"Notion","https://notion.so",
+				"All in one tool to organize teams and ideas. Write, plan, collaborate, and get organized.",
+				list1);
+		
+		Ferramenta f2 = new Ferramenta(null,"json-server" , "https://github.com/typicode/json-server", 
+				"Fake REST API based on a json schema. Useful for mocking and creating APIs for front-end devs to consume in coding challenges.", list2);
+		
+		ferramentaRepository.saveAll(Arrays.asList(f1,f2));
+	}
+	
+	
+
+}
