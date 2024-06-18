7a1f33934ea852d77d28e60f189b19ea5b757566
Rodrigo Oliveira <rodrigogitversion@gmail.com>
2024-06-18 13:47:43 -0300

    Entity class

---
+++ src/main/java/com/desafio/bossabox/entities/Ferramenta.java
@@ -0,0 +1,92 @@
+package com.desafio.bossabox.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_ferramenta")
+public class Ferramenta {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String title;
+	private String link;
+	private String description;
+	private List<String> tags = new ArrayList<>();
+	
+	public Ferramenta(Long id, String title, String link, String description, List<String> tags) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.link = link;
+		this.description = description;
+		this.tags = tags;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getLink() {
+		return link;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public List<String> getTags() {
+		return tags;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Ferramenta other = (Ferramenta) obj;
+		return Objects.equals(id, other.id);
+	}
+
+	
+	
+	
+	
+	
+}
